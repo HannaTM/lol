4b23e2af94084668f95cb81905e24f58285fd15f
HannaTM <89134188+HannaTM@users.noreply.github.com>
2021-08-19 10:17:29 +0700

    Add files via upload

---
+++ lgneofr;kfmnfi2jf.py
@@ -0,0 +1,8 @@
+str = input()
+if int(str[0]) + int(str[1]) + int(str[2]) == int(str[3]) + int(str[4]) + int(str[5]):
+    print('Счастливый')
+else:
+    print('Обычный')
+
+C:\Users\Никита\PycharmProjects\Duck_reseachr1\venv\lgneofr;kfmnfi2jf.py
+C:\Users\Никита\PycharmProjects\Duck_reseachr1\venv\lgneofr;kfmnfi2jf.py
